935ce1a2fa49a6e0cc3fc416766c13b43663d714
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-22 13:12:19 -0400

    adds dummy background jobs module

--- programs/phx_hello/hello/lib/hello_web/router.ex
+++ programs/phx_hello/hello/lib/hello_web/router.ex
@@ -19,6 +19,7 @@ defmodule HelloWeb.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    forward "/jobs", BackgroundJob.Plug, name: "Hello Friend"
   end
 
   # Other scopes may use custom stacks.
