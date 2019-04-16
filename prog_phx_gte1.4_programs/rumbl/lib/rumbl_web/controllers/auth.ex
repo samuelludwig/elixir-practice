b6e41dbe45bae9291f822af67a6555361203e889
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-04-16 07:58:06 -0400

    adds user login and logout functionality

--- prog_phx_gte1.4_programs/rumbl/lib/rumbl_web/controllers/auth.ex
+++ prog_phx_gte1.4_programs/rumbl/lib/rumbl_web/controllers/auth.ex
@@ -11,11 +11,24 @@ defmodule RumblWeb.Auth do
     assign(conn, :current_user, user)
   end
 
+  @spec login(Plug.Conn.t(), atom() | %{id: any()}) :: Plug.Conn.t()
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  def login_by_email_and_pass(conn, email, given_pass) do
+    case Accounts.authenticate_by_email_and_pass(email, given_pass) do
+      {:ok, user} -> {:ok, login(conn, user)}
+      {:error, :unauthorized} -> {:error, :unauthorized, conn}
+      {:error, :not_found} -> {:error, :not_found, conn}
+    end
+  end
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
   end
 
 end
