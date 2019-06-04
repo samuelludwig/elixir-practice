77dad6cd31fcba8e4cc152fd3aafd926b8505503
samuelludwig <samuelludwig74@yahoo.com>
2019-06-04 08:27:46 -0400

    Add everything from initial walkthrough on ES.

--- lv_testdrive_umbrella/apps/lv_testdrive_web/lib/lv_testdrive_web/live/github_deploy_view.ex
+++ lv_testdrive_umbrella/apps/lv_testdrive_web/lib/lv_testdrive_web/live/github_deploy_view.ex
@@ -2,16 +2,15 @@ defmodule LvTestdriveWeb.GithubDeployView do
   use Phoenix.LiveView
 
   def render(assigns) do
-    ~L"""
-    <div class="">
-      <div>
-        <%= @deploy_step %>
-      </div>
-    </div>
-    """
+    LvTestdriveWeb.PageView.render("github_deploy.html", assigns)
   end
 
   def mount(_session, socket) do
     {:ok, assign(socket, deploy_step: "Ready!")}
+  end
+
+  def handle_event("github_deploy", _value, socket) do
+    # do the deploy process
+    {:noreply, assign(socket, deploy_step: "Starting deploy...")}
   end
 end
