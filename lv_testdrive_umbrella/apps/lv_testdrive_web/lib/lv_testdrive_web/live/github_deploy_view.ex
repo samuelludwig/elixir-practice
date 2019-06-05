850348365f96bfbd684a2833ea32f7786f635f75
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-06-05 09:15:59 -0400

    Revert "Add everything from initial walkthrough on ES."
    This reverts commit 77dad6cd31fcba8e4cc152fd3aafd926b8505503.
    
    undo local changes

--- lv_testdrive_umbrella/apps/lv_testdrive_web/lib/lv_testdrive_web/live/github_deploy_view.ex
+++ lv_testdrive_umbrella/apps/lv_testdrive_web/lib/lv_testdrive_web/live/github_deploy_view.ex
@@ -2,15 +2,16 @@ defmodule LvTestdriveWeb.GithubDeployView do
   use Phoenix.LiveView
 
   def render(assigns) do
-    LvTestdriveWeb.PageView.render("github_deploy.html", assigns)
+    ~L"""
+    <div class="">
+      <div>
+        <%= @deploy_step %>
+      </div>
+    </div>
+    """
   end
 
   def mount(_session, socket) do
     {:ok, assign(socket, deploy_step: "Ready!")}
-  end
-
-  def handle_event("github_deploy", _value, socket) do
-    # do the deploy process
-    {:noreply, assign(socket, deploy_step: "Starting deploy...")}
   end
 end
