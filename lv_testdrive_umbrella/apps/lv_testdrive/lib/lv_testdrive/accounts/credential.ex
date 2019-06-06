346ee8e56af11892b204cc5f850358270a693f6f
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-06-05 20:29:42 -0400

    Add initial Accounts context.

---
+++ lv_testdrive_umbrella/apps/lv_testdrive/lib/lv_testdrive/accounts/credential.ex
@@ -0,0 +1,31 @@
+defmodule LvTestdrive.Accounts.Credential do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "credentials" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    belongs_to :user, LvTestdrive.Accounts.User
+
+    timestamps()
+  end
+
+  def changeset(credential, attrs) do
+    credential
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> unique_constraint(:email)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
+  end
+end
