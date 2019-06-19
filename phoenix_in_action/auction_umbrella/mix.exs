4e1244736020029db933e4677036bcfa32e3a4ca
samuelludwig <samuelludwig74@yahoo.com>
2019-06-19 13:09:23 -0400

    Add umbrella project structure and dependencies for 'Auction' app.

---
+++ phoenix_in_action/auction_umbrella/mix.exs
@@ -0,0 +1,23 @@
+defmodule AuctionUmbrella.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, "~> 0.14.1"}
+    ]
+  end
+end
