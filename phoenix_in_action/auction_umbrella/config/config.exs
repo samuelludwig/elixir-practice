68e70e416db642e98e6bc279c27834653d17c7b3
samuelludwig <samuelludwig74@yahoo.com>
2019-06-21 12:04:01 -0400

    Implement ability to grab and display content from `Auction.FakeRepo`.

--- phoenix_in_action/auction_umbrella/config/config.exs
+++ phoenix_in_action/auction_umbrella/config/config.exs
@@ -2,6 +2,16 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :auction_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :auction_web, AuctionWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "N1+4k4grz0Vd7B0vceqQDdJ6YYryI97hyLSsTeVdqVGmNyz77Yq9nXSTmqkexDzz",
+  render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
 # By default, the umbrella project as well as each child
 # application will require this configuration file, as
 # configuration and dependencies are shared in an umbrella
@@ -18,3 +28,15 @@ import_config "../apps/*/config/config.exs"
 #       level: :info,
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
