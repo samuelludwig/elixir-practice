a5928b2161e5a5f8c8b0a76d75fea8e6abcb081b
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-06-22 20:20:35 -0400

    Add initial files for nerves_skeleton project.

---
+++ nerves_skeleton/lib/nerves_skeleton.ex
@@ -0,0 +1,18 @@
+defmodule NervesSkeleton do
+  @moduledoc """
+  Documentation for NervesSkeleton.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesSkeleton.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ nerves_skeleton/test/nerves_skeleton_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesSkeletonTest do
+  use ExUnit.Case
+  doctest NervesSkeleton
+
+  test "greets the world" do
+    assert NervesSkeleton.hello() == :world
+  end
+end
